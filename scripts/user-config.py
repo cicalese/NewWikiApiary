662d7ba155c11945d096d0e60a335a9912d2786c
cicalese <cindom@gmail.com>
2024-01-01 09:24:08 -0500

    tweaked login

--- scripts/user-config.py
+++ scripts/user-config.py
@@ -2,7 +2,7 @@ import os
 
 mylang = 'wikiapiary'
 family = 'wikiapiary'
-usernames['wikiapiary']['*'] = os.environ['WIKIAPIARY_BOT_USERNAME']
+usernames['wikiapiary']['*'] = os.environ['WIKIAPIARY_USERNAME']
 password_file='user-password.py'
 family_files['wikiapiary'] =  os.environ['WIKIAPIARY_URL']
 put_throttle = 0
