9a58b306cda65917cfe87707063640af0ff3f8bb
designburo <info@designburo.nl>
2024-01-24 20:38:56 +0100

    Fixed Top extensions ( not skins yet )

--- includes/data/query/Stats.php
+++ includes/data/query/Stats.php
@@ -78,11 +78,31 @@ ORDER BY
     COUNT
 DESC
     ;
+
+		SELECT w8y_extension_data.w8y_ed_name, COUNT(w8y_extension_data.w8y_ed_ed_id) as count
+FROM w8y_wikis
+JOIN w8y_scrape_records ON w8y_wikis.w8y_wi_last_sr_id = w8y_scrape_records.w8y_sr_sr_id
+JOIN w8y_extension_links ON w8y_scrape_records.w8y_sr_vr_id = w8y_extension_links.w8y_el_vr_id
+JOIN w8y_extension_data ON w8y_extension_links.w8y_el_ed_id = w8y_extension_data.w8y_ed_ed_id
+WHERE w8y_wikis.w8y_wi_is_defunct = 0
+GROUP BY w8y_ed_name
+ORDER BY count DESC;
 		 */
-		$select = [ '*',
-			'count' => 'count(*)' ];
-		$from = Structure::DBTABLE_EXTENSIONS;
-		$res = $dbr->newSelectQueryBuilder()->select( $select )->from( $from )->groupBy( 'w8y_ex_name' )
+
+
+		$select = [ Structure::DBTABLE_EXTENSIONS . '.*',
+			'count' => 'count(' . Structure::DBTABLE_EXTENSIONS . '.' . Structure::EXTENSION_ID . ')' ];
+		$from = Structure::DBTABLE_WIKIS;
+		$res = $dbr->newSelectQueryBuilder()->select( $select )->from( $from )->
+		leftJoin( Structure::DBTABLE_SCRAPE,
+		null,
+		Structure::DBTABLE_WIKIS . '.' . Structure::WIKI_LAST_SR_RCRD . '=' . Structure::DBTABLE_SCRAPE . '.' . Structure::SR_ID )->
+		leftJoin( Structure::DBTABLE_EXTENSIONS_LINK,
+		null,
+			Structure::DBTABLE_SCRAPE . '.' . Structure::SCRAPE_VR_ID . '=' . Structure::DBTABLE_EXTENSIONS_LINK . '.' . Structure::EXTENSION_LINK_VID )->
+		leftJoin( Structure::DBTABLE_EXTENSIONS, null,
+			Structure::DBTABLE_EXTENSIONS_LINK . '.' . Structure::EXTENSION_LINK_ID . '=' . Structure::DBTABLE_EXTENSIONS . '.' . Structure::EXTENSION_ID )->
+		groupBy( Structure::EXTENSION_ID )
 			->orderBy( 'count',
 				'DESC' )->limit( $limit )->caller( __METHOD__ )->fetchResultSet();
 		$ret = [];
