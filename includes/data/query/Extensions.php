d4840d8c23d91be5eaa11ef94c8d57ff962eda11
designburo <info@designburo.nl>
2024-02-01 19:33:49 +0100

    Added usedby query for extensions.

--- includes/data/query/Extensions.php
+++ includes/data/query/Extensions.php
@@ -117,6 +117,62 @@ class Extensions {
 
 	/**
 	 * @param string $extensionName
+	 * @param DBConnRef $dbr
+	 *
+	 * @return array
+	 */
+	private function getExtensionWiki( string $extensionName, DBConnRef $dbr ) {
+		$select = [ Structure::WIKI_PAGEID ];
+		$from = Structure::DBTABLE_WIKIS;
+		$where = Structure::EXTENSION_NAME . ' LIKE "' . $extensionName . '"';
+		// SELECT w8y_wi_page_id
+		// FROM w8y_wikis
+		// JOIN w8y_scrape_records ON w8y_wi_last_sr_id = w8y_sr_sr_id
+		// JOIN w8y_extension_links ON w8y_sr_vr_id = w8y_el_vr_id
+		// JOIN w8y_extension_data ON w8y_el_ed_id = w8y_ed_ed_id
+		// WHERE w8y_ed_name like "ParserFunctions";
+		try {
+			$res = $dbr->newSelectQueryBuilder()->
+			select( $select )->
+			from( $from )->
+			leftJoin( Structure::DBTABLE_SCRAPE, null, Structure::DBTABLE_WIKIS . '.' . Structure::WIKI_LAST_SR_RCRD . '=' . Structure::DBTABLE_SCRAPE . '.' . Structure::SR_ID )->
+			leftJoin( Structure::DBTABLE_EXTENSIONS_LINK, null, Structure::DBTABLE_SCRAPE . '.' . Structure::SCRAPE_VR_ID . '=' . Structure::DBTABLE_EXTENSIONS_LINK . '.' . Structure::EXTENSION_LINK_VID )->
+			leftJoin( Structure::DBTABLE_EXTENSIONS, null, Structure::DBTABLE_EXTENSIONS_LINK . '.' . Structure::EXTENSION_LINK_ID . '=' . Structure::DBTABLE_EXTENSIONS . '.' . Structure::EXTENSION_ID )->
+			where( $where )->
+			caller( __METHOD__ )->
+			fetchResultSet();
+		} catch ( \Exception $e ) {
+			wfDebug( $e->getMessage(), 'w8y' );
+			return [];
+		}
+		$ret = [];
+		$t = 0;
+		if ( $res->numRows() > 0 ) {
+			while ( $row = $res->fetchRow() ) {
+				$ret[$t][Structure::WIKI_PAGEID] = $row[Structure::WIKI_PAGEID];
+				$ret[$t]['w8y_pageTitle'] = $this->getPageTitleFromId( $row[Structure::WIKI_PAGEID] );
+				$t++;
+			}
+		}
+
+		return $ret;
+	}
+
+	/**
+	 * @param int $id
+	 *
+	 * @return string
+	 */
+	private function getPageTitleFromId( int $id ): string {
+		$page = MediaWikiServices::getInstance()->getWikiPageFactory()->newFromID( $id );
+		if ( $page === null ) {
+			return '';
+		}
+		return $page->getTitle()->getFullText();
+	}
+
+	/**
+	 * @param string $extensionName
 	 * @param string $queryType
 	 * @param string $export
 	 *
@@ -139,6 +195,12 @@ class Extensions {
 				$result = $this->getExtensionDocumentation( $extensionName, $dbr );
 				$tables = [ Structure::w8yMessage( Structure::EXTENSION_DOC_URL ),
 					Structure::w8yMessage( 'w8y_count' )
+				];
+				break;
+			case "usedby":
+				$result = $this->getExtensionWiki( $extensionName, $dbr );
+				$tables = [ Structure::w8yMessage( Structure::WIKI_PAGEID ),
+					Structure::w8yMessage( 'w8y_pageTitle' )
 				];
 				break;
 			default:
